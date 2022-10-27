5de206564d14f1bf06d95492f1ba7191905b8d36
rudra-1710 <rudrachittturi@outlook.com>
2022-10-27 22:47:35 +0530

    done of the day

---
+++ healingStones.js
@@ -0,0 +1,177 @@
+let healing_stone_products = [
+    {
+        id:1,
+        SKU:"GS012",
+        stock:6,
+        DESCRIPTION:"SEVEN CHAKRA SALENITE PLATE",
+        image_url: "",
+        Dimenstion:"11x11x3.00cm",
+        price:"Rs 500"
+    },
+    {
+        id:2,
+        SKU:"GS015",
+        stock:6,
+        DESCRIPTION:"ROSE QUARTZ MASSAGER",
+        image_url: "",
+        Dimenstion:"118x8x2.5cm",
+        price:"Rs 500"
+    },
+    {
+        id:3,
+        SKU:"GS091",
+        stock:6,
+        DESCRIPTION:"SEVEN CHAKRA STONES",
+        image_url: "",
+        Dimenstion:"20x11x3.5cm",
+        price:"Rs 750"
+    },
+    {
+        id:4,
+        SKU:"GS0100",
+        stock:6,
+        DESCRIPTION:"BLACK ONEX PYRAMID",
+        image_url: "",
+        Dimenstion:"6.5x6.5x4cm",
+        price:"Rs 300"
+    },
+    {
+        id:5,
+        SKU:"GS061",
+        stock:6,
+        DESCRIPTION:"SEVEN CHAKRA HEALING STICK",
+        image_url: "",
+        Dimenstion:"24x4.5x3cm",
+        price:"Rs 500"
+    },
+    {
+        id:6,
+        SKU:"GS051",
+        stock:6,
+        DESCRIPTION:"SEVEN CHAKRA PYRAMID",
+        image_url: "",
+        Dimenstion:"7.5x7.5x5.5cm",
+        price:"Rs 300"
+    },
+    {
+        id:7,
+        SKU:"GS07",
+        stock:6,       
+        DESCRIPTION:"SEVEN CHAKRA SALENITE SCALE",
+        image_url: "",
+        Dimenstion:"24x4.5x3cm",
+        price:"Rs 500"
+    },
+    {
+        id:8,
+        SKU:"GS041",
+        stock:6,
+        DESCRIPTION:"SEVEN CHAKRA PYRAMID",
+        image_url: "",
+        Dimenstion:"20x11x3.5cm",
+        price:"Rs 500"
+    },
+    {
+        id:9,
+        SKU:"GS031",
+        stock:6,
+        DESCRIPTION:"SEVEN CHAKRA HEALING CODES",
+        image_url: "",
+        Dimenstion:"20x11x3.5cm",
+        price:"Rs 750"
+    },
+    {
+        id:10,
+        SKU:"GS021",
+        stock:6,
+        DESCRIPTION:"SEVEN CHAKRA POCKET BOTTLES",
+        image_url: "",
+        Dimenstion:"33x8x5cm",
+        price:"Rs 1000"
+    },
+
+];
+
+let necklace_products = [
+    {
+        id:1,
+        style:"GS-111",
+        DESCRIPTION:"AMETHYST & AGATE",
+        image_url:"",
+        remark:"Rs 2500",
+        length:"18 INCH"
+    },
+    {
+        id:2,
+        style:"GS-112",
+        DESCRIPTION:"SHELL PEARL AND SMOKY",
+        image_url:"",
+        remark:"Rs 1200",
+        length:"18 INCH"
+    },
+    {
+        id:3,
+        style:"GS-113",
+        DESCRIPTION:"ROSE QUARTZ, PEARL & MONALISSA",
+        image_url:"",
+        remark:"Rs 4500",
+        length:"18 INCH"
+    },
+    {
+        id:4,
+        style:"GS-114",
+        DESCRIPTION:"BLACK SPINAL AND GREY PEARL",
+        image_url:"",
+        remark:"Rs 4500",
+        length:"25 INCH"
+    },
+    {
+        id:5,
+        style:"GS-115",
+        DESCRIPTION:"AQUA AND PERAL",
+        image_url:"",
+        remark:"Rs 7500",
+        length:"20 INCH"
+    },
+    {
+        id:6,
+        style:"GS-116",
+        DESCRIPTION:"AQUA AND PERAL",
+        image_url:"",
+        remark:"Rs 4500",
+        length:"22 INCH"
+    },
+    {
+        id:7,
+        style:"GS-117",
+        DESCRIPTION:"MASH AND TIGER EYE",
+        image_url:"",
+        remark:"Rs 1200",
+        length:"14 INCH"
+    },
+    {
+        id:8,
+        style:"GS-118",
+        DESCRIPTION:"QUARTZ AND PEARL",
+        image_url:"",
+        remark:"Rs 1500",
+        length:"15 INCH"
+    },
+    {
+        id:9,
+        style:"GS-119",
+        DESCRIPTION:"BLUE QUARTZ AND MOON STONES",
+        image_url:"",
+        remark:"Rs 1500",
+        length:"19 INCH"
+    },
+    {
+        id:10,
+        style:"GS-120",
+        DESCRIPTION:"TURQUIOSE",
+        image_url:"",
+        remark:"Rs 3000",
+        length:"18 INCH"
+    },
+
+]
